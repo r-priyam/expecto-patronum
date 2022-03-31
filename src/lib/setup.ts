4f01b2e1a3ae93cac54d3f4ee9ae7a98cea6ed51
Priyam <priyamroy29@gmail.com>
2022-03-31 08:59:28 +0530

    chore: add application command overwrite

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -5,6 +5,11 @@ import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
 import * as colorette from 'colorette';
 import { inspect } from 'util';
+import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
+
+// set behavior to overwrite so that it can be overwritten by other changes
+// rather than warning in console.
+ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
 
 inspect.defaultOptions.depth = 2;
 colorette.createColors({ useColor: true });
