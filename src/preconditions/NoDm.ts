4bc81e4085a65ba694ad45755d21d23a10248aec
Priyam <priyamroy29@gmail.com>
2022-04-02 09:03:10 +0530

    chore: add global check `DM` command/interaction

---
+++ src/preconditions/NoDm.ts
@@ -0,0 +1,22 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { AllFlowsPrecondition, Precondition } from '@sapphire/framework';
+import type { CommandInteraction, ContextMenuInteraction, Message } from 'discord.js';
+
+@ApplyOptions<Precondition.Options>({ position: 1 })
+export class UserPrecondition extends AllFlowsPrecondition {
+	public async chatInputRun(interaction: CommandInteraction) {
+		return this.checkGuild(interaction.inGuild());
+	}
+
+	public async messageRun(message: Message) {
+		return this.checkGuild(message.inGuild());
+	}
+
+	public async contextMenuRun(interaction: ContextMenuInteraction) {
+		return this.checkGuild(interaction.inGuild());
+	}
+
+	private async checkGuild(guild: boolean) {
+		return guild ? this.ok() : this.error({ message: "Commands in DM aren't supported" });
+	}
+}
