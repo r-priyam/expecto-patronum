195a90f01024f0b27ea3df8bed70b44f99e1df86
Priyam <priyamroy29@gmail.com>
2022-04-08 21:18:45 +0530

    feat: setup `postgres.js`

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -2,8 +2,12 @@ import '#root/config';
 import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
 
-import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { ApplicationCommandRegistries, container, RegisterBehavior } from '@sapphire/framework';
 import * as colorette from 'colorette';
+import { blueBright, cyan, greenBright, redBright, yellow } from 'colorette';
+import type { Sql } from 'postgres';
+import postgres from 'postgres';
 import { inspect } from 'util';
 
 // set behavior to overwrite so that it can be overwritten by other changes
@@ -12,3 +16,31 @@ ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior
 
 inspect.defaultOptions.depth = 1;
 colorette.createColors({ useColor: true });
+
+const sqlHighlighter = new SqlHighlighter();
+container.sql = postgres({
+	debug: (connection, query, params, types) => {
+		container.logger.debug(
+			`${blueBright('Connections:')} ${yellow(connection)} » ${greenBright('Query:')} ${sqlHighlighter.highlight(query)} » ${redBright(
+				'Params:'
+			)} ${yellow(String(params || 'NULL'))} » ${cyan('Types:')} ${yellow(String(types || 'NULL'))}`
+		);
+	},
+	transform: {
+		column: { to: postgres.fromCamel, from: postgres.toCamel }
+	},
+	types: {
+		date: {
+			to: 1184,
+			from: [1082, 1083, 1114, 1184],
+			serialize: (date: Date) => date.toISOString(),
+			parse: (isoString) => isoString
+		}
+	}
+});
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		sql: Sql<any>;
+	}
+}
