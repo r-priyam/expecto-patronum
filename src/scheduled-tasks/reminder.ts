434f3efa2f493306b3676f3310188340774faabd
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-04-26 13:46:52 +0530

    fix: handle expired reminders

--- src/scheduled-tasks/reminder.ts
+++ src/scheduled-tasks/reminder.ts
@@ -17,8 +17,37 @@ export class ReminderTask extends ScheduledTask {
 			);
 		}
 
-		await this.sql`DELETE FROM reminders WHERE id = ${payload.id}`;
+		await this.sql`DELETE
+					   FROM reminders
+					   WHERE id = ${payload.id}`;
 	}
+
+	public override async onLoad() {
+		// If bot goes offline, i.e for maintenance or whatsoever then it may miss the
+		// scheduled reminders by few seconds so check for pending reminders here whenever
+		// bot starts and dispatch them.
+		const pending = await this.sql<RawReminderPayload[]>`SELECT id, user_id, message, created
+															 FROM reminders
+															 WHERE expires < (CURRENT_DATE + ${'2 days'}::interval)
+															 ORDER BY expires`;
+		if (pending.length > 0) {
+			for (const reminder of pending) {
+				await this.run({
+					id: reminder.id,
+					userId: reminder.user_id,
+					message: reminder.message,
+					createdAt: String(reminder.created)
+				});
+			}
+		}
+	}
+}
+
+interface RawReminderPayload {
+	id: number;
+	user_id: string;
+	message: string;
+	created: Date;
 }
 
 interface ReminderPayload {
