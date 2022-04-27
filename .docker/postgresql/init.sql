2c3d9d3b5a303f50725d447d5ba479a2b7c831f9
r-priyam <50884372+r-priyam@users.noreply.github.com>
2022-04-27 06:54:24 +0530

    fix: column names

--- .docker/postgresql/init.sql
+++ .docker/postgresql/init.sql
@@ -11,10 +11,10 @@ CREATE TABLE public.reminders
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-CREATE INDEX idx_reminders_expires ON reminders USING BTREE (expires ASC);
+CREATE INDEX idx_reminders_expires ON reminders USING BTREE (expires_at ASC);
 
 COMMENT ON COLUMN public.reminders.id IS 'The unique identifier for the reminder.';
 COMMENT ON COLUMN public.reminders.user_id IS 'The user ID of the user who created the reminder.';
 COMMENT ON COLUMN public.reminders.message IS 'The message to be sent to the user.';
-COMMENT ON COLUMN public.reminders.created IS 'The date and time the reminder was created.';
-COMMENT ON COLUMN public.reminders.expires IS 'The date and time the reminder expires.';
+COMMENT ON COLUMN public.reminders.created_at IS 'The date and time the reminder was created.';
+COMMENT ON COLUMN public.reminders.expires_at IS 'The date and time the reminder expires.';
